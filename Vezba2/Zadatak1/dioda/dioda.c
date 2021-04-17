68a73f46ae8f101d431cc747c4f7cb3689c837c3
alekssandra8 <savic838@gmail.com>
2021-04-17 20:45:49 +0200

    pod c)

--- Vezba2/Zadatak1/dioda/dioda.c
+++ Vezba2/Zadatak1/dioda/dioda.c
@@ -1,16 +1,34 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
+
+// pod c) Koriscenje tajmera0, ali pomocu  nove funkcije
+
+void my_delay(unsigned long ms)
+{
+
+}
+
+unsigned long ms = 0;
+
+ISR(TIMER0_COMPA_vect)
+{
+	ms++;
+}
 
 int main()
 {
 	DDRB |= 1 << 5;
+
+	TCCR0A = 0x02;
+	TCCR0B = 0x03;
+	OCR0A = 249;
+	TIMSK0 = 0x02;
+
+	sei();
+
 	while(1)
 	{
-		PORTB |= 1 << 5;
-		_delay_ms(1000);
 
-		PORTB &= ~(1 << 5);
-		_delay_ms(1000);
 	};
 return 0;
 }
