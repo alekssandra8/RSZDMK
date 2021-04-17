8598d9756d530154371c2c1ddb74f7ae9cd20901
alekssandra8 <savic838@gmail.com>
2021-04-17 20:24:33 +0200

    pod b)

--- Vezba2/Zadatak1/dioda/dioda.c
+++ Vezba2/Zadatak1/dioda/dioda.c
@@ -1,16 +1,34 @@
 #include <avr/io.h>
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
+unsigned long ms = 0;
+
+ISR(TIMER0_COMPA_vect)
+{
+	ms++;
+}
 int main()
 {
+	unsigned long t0;
+
 	DDRB |= 1 << 5;
+
+	TCCR0A = 0x02;
+	TCCR0B = 0x03;
+	OCR0A = 249;
+	TIMSK0 = 0x02;
+
+	sei();
+
 	while(1)
 	{
+		t0 = ms;
+		while ((ms - t0) < 500);
 		PORTB |= 1 << 5;
-		_delay_ms(1000);
 
+		t0 = ms;
+		while ((ms - t0) < 500);
 		PORTB &= ~(1 << 5);
-		_delay_ms(1000);
 	};
 return 0;
 }
