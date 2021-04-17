cd0f45c67cea14be0cf1f441e7a0910a79d58549
alekssandra8 <savic838@gmail.com>
2021-04-17 20:05:32 +0200

    pod a)

--- Vezba2/Zadatak1/dioda/dioda.c
+++ Vezba2/Zadatak1/dioda/dioda.c
@@ -1,16 +1,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
-
+// pod a) Treperenje diode frekvencijom 2Hz, koriscenjem _delay_ms()
 int main()
 {
 	DDRB |= 1 << 5;
 	while(1)
 	{
 		PORTB |= 1 << 5;
-		_delay_ms(1000);
+		_delay_ms(500);
 
 		PORTB &= ~(1 << 5);
-		_delay_ms(1000);
+		_delay_ms(500);
 	};
 return 0;
 }
